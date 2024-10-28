45427c80b83225f4888cec2697a7197f4960a226
Haitham Shahin <42976403+haithamshahin333@users.noreply.github.com>
2024-10-28 15:42:17 +0000

    adding shinyproxy-operator as folder in repo

---
+++ shiny-config/shinyproxy-operator/docs/prometheus.md
@@ -0,0 +1,5 @@
+# Prometheus Usage Statistics
+
+Check out the
+[ShinyProxy Monitoring Stack](https://github.com/openanalytics/shinyproxy-monitoring)
+for a ready to use deployment of Prometheus, Loki and Grafana.
---
+++ shiny-config/shinyproxy-operator/src/main/kotlin/eu/openanalytics/shinyproxyoperator/main.kt
@@ -0,0 +1,45 @@
+/**
+ * ShinyProxy-Operator
+ *
+ * Copyright (C) 2021-2024 Open Analytics
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
+package eu.openanalytics.shinyproxyoperator
+
+import eu.openanalytics.shinyproxyoperator.crd.ShinyProxy
+import io.fabric8.kubernetes.api.model.KubernetesResourceList
+import io.fabric8.kubernetes.client.KubernetesClientException
+import io.fabric8.kubernetes.client.dsl.MixedOperation
+import io.fabric8.kubernetes.client.dsl.Resource
+import mu.KotlinLogging
+import kotlin.system.exitProcess
+
+typealias ShinyProxyClient = MixedOperation<ShinyProxy, KubernetesResourceList<ShinyProxy>, Resource<ShinyProxy>>
+
+suspend fun main() {
+    val logger = KotlinLogging.logger {}
+    try {
+        val operator = Operator()
+        Operator.setOperatorInstance(operator)
+        val (resourceRetriever, shinyProxyLister) = operator.prepare()
+        operator.run(resourceRetriever, shinyProxyLister)
+    } catch (exception: KubernetesClientException) {
+        logger.warn { "Kubernetes Client Exception : ${exception.message}" }
+        exception.printStackTrace()
+        exitProcess(1)
+    }
+}
