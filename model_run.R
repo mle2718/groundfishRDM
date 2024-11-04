a1a8354654c27c4ec5c57060fa90bb78e65bf30c
kimberly-bastille <kimberly.bastille@noaa.gov>
2024-11-04 16:28:36 -0500

    too many workers

--- model_run.R
+++ model_run.R
@@ -54,7 +54,7 @@ mrip_index  <- mrip_index[1:400] ## For testing should be all when ready for fin
 # pred<- NULL
 # for (x in mrip_index){
 #future::plan(future::multisession, workers = 6)
-future::plan(future::multisession, workers = 150)
+future::plan(future::multisession, workers = 120)
 get_predictions_out<- function(x){
 
   baseline_comparison<-baseline_comparison1 %>%
