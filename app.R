212e62efbf61045a3232d9c2eddf56b56ef21688
mle2718 <Min-Yang.Lee@noaa.gov>
2024-12-04 20:18:09 -0500

    A little extra writing.

--- app.R
+++ app.R
@@ -21,7 +21,12 @@ ui <- fluidPage(
                a table of recreational management measures. The second section contains graphs of mortality. The third
                section has graphs of other performance measures, including Economic Surplus, Trips, and Discards."),
 
-             p("This figure plots the predicted Cod and Haddock recreational mortality for previously simulated management measures."),
+
+             p("The first figure plots the predicted Cod and Haddock recreational mortality for previously simulated management measures."),
+
+             p("Cod mortality is plotted on the horizontal axis. The Cod ACL is the dashed line. Regulations with average mortality under the cod ACL are to the left of the dashed vertical line."),
+
+             p("Haddock mortality is plotted on the vertical axis. The Haddock ACL is the solid line. Regulations with average mortality under the Haddock ACL are below the solid horizontal line."),
              plotlyOutput(outputId = "totCatch"),
 
              DTOutput(outputId = "DTout"),
@@ -30,7 +35,7 @@ ui <- fluidPage(
 
              shinyWidgets::awesomeCheckboxGroup(
                inputId = "fig",
-               label = "Supplimental Figures",
+               label = "Supplemental Figures",
                choices = c( "Consumer Surplus","Releases", "Trips"),
                inline = TRUE,
                status = "danger"),
