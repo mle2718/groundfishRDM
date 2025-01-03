071ba4ceb34bfdcb39a334c614e60917bdede3d1
kimberly-bastille <kimberly.bastille@noaa.gov>
2025-01-03 12:42:13 -0500

    Set full run

--- model_run.R
+++ model_run.R
@@ -170,13 +170,13 @@ dplyr::n_distinct(baseline_comparison1$draw)
 calendar_adjust1 <- readr::read_csv(here::here("data-raw/next year calendar adjustments.csv"), show_col_types = FALSE)
 
 mrip_index <- c(unique(baseline_comparison1$mrip_index))
-#mrip_index  <- mrip_index[1:400]
-mrip_index  <- mrip_index[1:4]
+mrip_index  <- mrip_index[1:400]
+#mrip_index  <- mrip_index[1:4]
 
  #pred<- NULL
  #for (x in mrip_index){
-future::plan(future::multisession, workers = 6)
-#future::plan(future::multisession, workers = 120)
+#future::plan(future::multisession, workers = 6)
+future::plan(future::multisession, workers = 120)
 get_predictions_out<- function(x){
 
   baseline_comparison<-baseline_comparison1 %>%
