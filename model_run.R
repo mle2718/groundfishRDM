b144961c2676973a1d81391551a4ec6a8148e9fd
kimberly-bastille <kimberly.bastille@noaa.gov>
2025-01-05 14:12:46 -0500

    workers = 150

--- model_run.R
+++ model_run.R
@@ -176,7 +176,7 @@ mrip_index  <- mrip_index[1:400]
  #pred<- NULL
  #for (x in mrip_index){
 #future::plan(future::multisession, workers = 6)
-future::plan(future::multisession, parallelly::availableCores())
+future::plan(future::multisession, workers = 150)
 get_predictions_out<- function(x){
 
   baseline_comparison<-baseline_comparison1 %>%
