7de5c56b560bb6fae3e1f25d20362eb918134ade
kimberly-bastille <kimberly.bastille@noaa.gov>
2025-01-10 16:34:33 -0500

    oops keep color steps

--- app.R
+++ app.R
@@ -424,7 +424,7 @@ server <- function(input, output, session){
       dplyr::mutate(under_acl_cod = as.numeric(under_acl_cod)) %>%
       ggplot2::ggplot(ggplot2::aes(x = `Cod Mortality`, y = `Haddock Mortality`, label = run_number))+
       ggplot2::geom_point(ggplot2::aes(colour = under_acl_cod)) +
-      #ggplot2::scale_color_stepsn(limits = c(0,100), n.breaks = 10, colors = my_palette, name = "% Under Cod ACL")+
+      ggplot2::scale_color_stepsn(limits = c(0,100), n.breaks = 10, colors = my_palette, name = "% Under Cod ACL")+
       ggplot2::geom_text( check_overlap = TRUE)+
       ggplot2::geom_vline( xintercept =cod_acl(), linetype="dashed")+
       ggplot2::geom_hline( yintercept =had_acl(), color="grey45")+
