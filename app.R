15e09683a086f26bd3baa1a48ce7b7abd0ecaae4
kimberly-bastille <kimberly.bastille@noaa.gov>
2025-01-10 17:12:14 -0500

    try to assign color in first aes

--- app.R
+++ app.R
@@ -233,25 +233,25 @@ server <- function(input, output, session){
   df2 <- function(){
     fnames <- list.files(path=here::here("output/"),pattern = "*.csv",full.names = T)
 
-    fnames2<- as.data.frame(fnames) %>%
-      tidyr::separate(fnames, into = c("a", "b"), sep = "_") %>%
-      dplyr::mutate(b = ifelse(stringr::str_detect(b, "202501"),  "NA", b),
-                    c=c(1:nrow(.)),
-                    run_name = dplyr::case_when(b != "NA" ~ b, TRUE ~ as.character(c))) %>%
-      dplyr::select(run_name)
-
     # fnames2<- as.data.frame(fnames) %>%
-    #   tidyr::separate(fnames, into = c("a", "b", "c"), sep = "_") %>%
-    #   dplyr::mutate(c = ifelse(stringr::str_detect(c, "202501"),  "NA", c),
-    #                 d=c(1:nrow(.)),
-    #                 run_name = dplyr::case_when(c != "NA" ~ c, TRUE ~ as.character(c))) %>%
+    #   tidyr::separate(fnames, into = c("a", "b"), sep = "_") %>%
+    #   dplyr::mutate(b = ifelse(stringr::str_detect(b, "202501"),  "NA", b),
+    #                 c=c(1:nrow(.)),
+    #                 run_name = dplyr::case_when(b != "NA" ~ b, TRUE ~ as.character(c))) %>%
     #   dplyr::select(run_name)
+
+    fnames2<- as.data.frame(fnames) %>%
+      tidyr::separate(fnames, into = c("a", "b", "c"), sep = "_") %>%
+      dplyr::mutate(c = ifelse(stringr::str_detect(c, "202501"),  "NA", c),
+                    d=c(1:nrow(.)),
+                    run_name = dplyr::case_when(c != "NA" ~ c, TRUE ~ as.character(c))) %>%
+      dplyr::select(run_name)
 
     df <- fnames %>%
       purrr::map_df(~data.table::fread(.,stringsAsFactors=F,check.names=T,strip.white=T))
 
 
-    df2<- df %>% dplyr::mutate(run_number = as.character(rep(fnames2$run_name, each = 6030)))
+    df2<- df %>% dplyr::mutate(run_number = as.character(rep(fnames2$run_name, each = 90)))
     return(df2)
   }
 
@@ -423,9 +423,10 @@ server <- function(input, output, session){
 
     p<- catch_agg %>%
       dplyr::mutate(under_acl_cod = as.numeric(under_acl_cod)) %>%
-      ggplot2::ggplot(ggplot2::aes(x = `Cod Mortality`, y = `Haddock Mortality`))+
-      ggplot2::geom_point(ggplot2::aes(colour = under_acl_cod)) +
-      ggplot2::scale_color_stepsn(limits = c(0,100), n.breaks = 10,
+      ggplot2::ggplot(ggplot2::aes(x = `Cod Mortality`, y = `Haddock Mortality`, color = under_acl_cod))+
+      ggplot2::geom_point() + #ggplot2::aes(colour = under_acl_cod)) +
+      ggplot2::scale_color_stepsn(
+        limits = c(0,100), n.breaks = 10,
                                   colors =  c("red3","red3","red3","red3","red3","#C5E8B7",
                                               "#ABE098", "#83D475","green4","darkgreen"),
                                   name = "% Under Cod ACL")+
