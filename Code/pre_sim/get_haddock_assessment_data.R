b417c7811d9436f20c65d8f0ac5d54647e115918
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-05-15 10:13:23 -0400

    add fishery to dataframe for dashboard

--- Code/pre_sim/get_haddock_assessment_data.R
+++ Code/pre_sim/get_haddock_assessment_data.R
@@ -61,6 +61,7 @@ data_version<-Sys.Date()
 
 # create a small dataframe that holds the stock "characteristics".
 stock_stats_df<-tibble(
+  fishery= "NE Groundfish",
   common = "HADDOCK",
   species_itis =164712 ,
   stock_abbrev = "GOM",
