62450e160fcd89475ecf811fa529cfac1d44ce24
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-05-20 09:37:55 -0400

    fix metric name

--- Code/pre_sim/get_haddock_assessment_data.R
+++ Code/pre_sim/get_haddock_assessment_data.R
@@ -414,7 +414,7 @@ historical_NAA <- historical_NAA %>%
 
 historical_NAA <-historical_NAA %>%
   cross_join(stock_stats_df)%>%
-  mutate(metric="historical mean Numbers At Age")
+  mutate(metric="Historical mean Numbers At Age")
 
 
 write_dta(historical_NAA, path=file.path(assessment_output_folder,glue("{HistoricalNAASaveFile}.dta")))
