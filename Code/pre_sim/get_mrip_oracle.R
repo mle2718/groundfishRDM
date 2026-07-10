e52b0f059ff9b1eec22b2804363b057a430104e1
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-07-10 11:40:22 -0400

    save dtas. set up to pass in first_yr and last_yr.

--- Code/pre_sim/get_mrip_oracle.R
+++ Code/pre_sim/get_mrip_oracle.R
@@ -17,12 +17,14 @@ here::i_am("Code/pre_sim/get_mrip_oracle.R")
 source(here("Code", "helpers", "developer_setup.R"))
 
 output_folder<-file.path(gf.data.dir, "miscellaneous")
-
+first_yr<-2023
+last_yr<-2026
 
 drv<-dbDriver("Oracle")
 con_name<-eval(nefscdb_con)
 
-yearlist<-2023:2026
+
+yearlist<-first_yr:last_yr
 wavelist<-1:6
 
 x <- mrip_microdata(
@@ -36,5 +38,26 @@ x <- mrip_microdata(
 # all lower case
 x <- map(x, ~ rename_with(.x, tolower))
 
-write_rds(x, file=file.path(output_folder, glue("mrip_pull.Rds")))
 # write this to an rds file.
+write_rds(x, file=file.path(output_folder, glue("mrip_pull.Rds")))
+
+# write this to an dtas file.
+write_dta(x$trip,
+          path=file.path(output_folder,
+                         glue("mrip_trip{first_yr}_{last_yr}.dta"))
+)
+
+write_dta(x$catch,
+          path=file.path(output_folder,
+              glue("mrip_catch{first_yr}_{last_yr}.dta"))
+          )
+
+write_dta(x$size,
+          path=file.path(output_folder,
+                         glue("mrip_size{first_yr}_{last_yr}.dta"))
+)
+write_dta(x$size_b2,
+          path=file.path(output_folder,
+                         glue("mrip_size_b2{first_yr}_{last_yr}.dta"))
+)
+
